ee88da7ff2ba4039d7ea45ba1795eb554fadd0d9
Cristian <cdwenz@gmail.com>
2024-07-01 15:02:30 -0300

    feat: create order

--- server.js
+++ server.js
@@ -36,6 +36,29 @@ app.post('/notification', (req, res) => {
     res.status(200).json({ status: 'received' });
 });
 
+app.post("/create_order", async (req, res) => {
+
+    try {
+      const response = await axios.post(
+        "https://api.mercadopago.com/instore/orders/qr/seller/collectors/52153870/pos/SUC001POS001/qrs",
+        req.body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.NODE_ENV_TOKEN}`,
+          },
+        }
+      );
+  
+      console.log("RESPONSE: ", response.data);
+  
+      res.json(response.data.qr_data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error creating order");
+    }
+  });
+
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
